638ffca6084b87fb6badfd2842edddcaa4384da0
Tony P <aphasay@outlook.com>
2025-11-03 18:27:29 -0700

    Remove unused imports for Netlify build

--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -3,8 +3,8 @@ import "./Contact.scss";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import {illustration, contactInfo} from "../../portfolio";
 import {Fade} from "react-reveal";
-import email from "../../assets/lottie/email";
-import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+// import email from "../../assets/lottie/email";
+// import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Contact() {
